0f7acad31f62e359e0737bf0ff7f2b66d3820ba8
iulisesq <iulisesq@hotmail.com>
2024-09-10 15:34:59 -0400

    electron

---
+++ android/app/src/main/java/io/github/gilbert/MainActivity.java
@@ -0,0 +1,5 @@
+package io.github.gilbert;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.gilbert',
+  appName: 'qui',
+  webDir: 'build'
+};
+
+export default config;
